58b82aeab62d823c94419d011612ec735fab6e06
phuong <phuong@phuongs-mbp-16.lan>
2024-05-15 12:41:20 -0400

    Test with note

--- demo_conflict.py
+++ demo_conflict.py
@@ -2,3 +2,5 @@ def demo_conflic():
   print("This is from ptruong88 demo conflict. Working on some changes. This is from allgames1988 user update 123.")
   print("This is from allgames1988 user update 123.")
   print("abc123. I just want to have more codes")
+  print("This is the logic I work on the branch stash_demo.")
+  print("I still work on it, but there is a bug to fix from the main branch.")
